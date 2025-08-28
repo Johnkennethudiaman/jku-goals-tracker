52d7488f3d0a823a3551a10cf2cf205937927e5e
medallorey2004 <medallorey27@gmail.com>
2025-08-28 11:43:55 +0800

    firebase Configuration

---
+++ firebaseConfig.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCkNTYvzj7UkGoUqDJR9ZJpuHpvcmgJimY",
+  authDomain: "jku-goals-tracker.firebaseapp.com",
+  projectId: "jku-goals-tracker",
+  storageBucket: "jku-goals-tracker.firebasestorage.app",
+  messagingSenderId: "987314705056",
+  appId: "1:987314705056:web:0838076c13303faaadefc5"
+};
+
+const app = initializeApp(firebaseConfig);
